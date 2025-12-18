4549ac7328b52bd47951c532cf61aa35d45b4bb2
Andy <test@email.com>
2025-12-18 06:04:28 -0500

    completed delete category method in categoryDAO

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -145,6 +145,21 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     public void delete(int categoryId)
     {
         // delete category
+        String sql = """
+                DELETE FROM
+                    categories
+                WHERE
+                    category_id = ?
+                """;
+        try(Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setInt(1, categoryId);
+            statement.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException("Error deleting category", e);
+        }
     }
 
     private Category mapRow(ResultSet row) throws SQLException

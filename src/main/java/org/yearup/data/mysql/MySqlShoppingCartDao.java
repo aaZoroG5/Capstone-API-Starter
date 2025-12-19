1af03275b4efcf4bd78cf27b4e7d659ec271385e
Andy <test@email.com>
2025-12-18 23:16:08 -0500

    completed getByUserId method in mySQL shoppingCArtDAO

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -2,9 +2,15 @@ package org.yearup.data.mysql;
 
 import org.springframework.stereotype.Component;
 import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
@@ -15,7 +21,61 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
 
     @Override
     public ShoppingCart getByUserId(int userId) {
-        return null;
+
+        //create shopping cart object
+        ShoppingCart cart = new ShoppingCart();
+
+        //create sql String
+        String sql = """
+                SELECT
+                    S.user_id,
+                    S.product_id,
+                    S.quantity,
+                    P.*
+                FROM
+                    shopping_cart S
+                JOIN
+                    products P ON (P.product_id = S.product_id)
+                WHERE
+                    S.user_id = ?
+                """;
+        //try/catch with resources
+        try(//create connection and preparedStatement
+            Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql))
+        {//set userId and run the query
+            statement.setInt(1, userId);
+            ResultSet result = statement.executeQuery();
+
+            while(result.next())
+            {//create new product
+                Product product = new Product();
+
+                //set all values for product
+                product.setProductId(result.getInt("product_id"));
+                product.setName(result.getString("name"));
+                product.setPrice(result.getBigDecimal("price"));
+                product.setCategoryId(result.getInt("category_id"));
+                product.setDescription(result.getString("description"));
+                product.setSubCategory(result.getString("subcategory"));//MISTAKE: misspelled label
+                product.setImageUrl(result.getString("image_url"));
+                product.setStock(result.getInt("stock"));
+                product.setFeatured(result.getBoolean("featured"));
+
+                //get quantity of product
+                int quantity = result.getInt("quantity");
+
+                //create cart item
+                ShoppingCartItem cartItem = new ShoppingCartItem();
+                cartItem.setProduct(product);
+                cartItem.setQuantity(quantity);
+
+                cart.add(cartItem);
+            }
+        }catch(SQLException e){
+            throw new RuntimeException("There was an error retrieving order by userId" + e);
+        }
+        return cart;
     }
 
     @Override

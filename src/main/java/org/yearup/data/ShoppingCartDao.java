8b3dffe3d1152df2bdf9352c1c943da97e9bbda6
Andy <test@email.com>
2025-12-18 22:05:32 -0500

    added signature methods to shoppingCartDAO

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -6,4 +6,9 @@ public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    void addToCart(int productId, int userId);
+
+    void clearCart(int userId);
+
+    public void editCart(int productId, int userId, int quantity);
 }

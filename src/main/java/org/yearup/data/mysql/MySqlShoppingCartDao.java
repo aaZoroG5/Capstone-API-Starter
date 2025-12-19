324f480b455e1b768ce678abb917f759d25b6323
Andy <test@email.com>
2025-12-18 22:20:55 -0500

    overrided methods from shoppingCartDAO into MySqlShoppingCart, added component annotation

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,4 +1,35 @@
 package org.yearup.data.mysql;
 
-public class MySqlShoppingCartDao {
+import org.springframework.stereotype.Component;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+
+import javax.sql.DataSource;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        return null;
+    }
+
+    @Override
+    public void addToCart(int productId, int userId) {
+
+    }
+
+    @Override
+    public void clearCart(int userId) {
+
+    }
+
+    @Override
+    public void editCart(int productId, int userId, int quantity) {
+
+    }
 }
